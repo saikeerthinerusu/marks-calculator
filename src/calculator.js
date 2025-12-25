78ac6e831d243d1fcc334b9dff8c2484c045dc35
Sai Keerthi Nerusu <saikeerthinerusu@gmail.com>
2025-12-25 18:01:14 +0530

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -1,26 +1,14 @@
-/**
- * Marks Calculator
- * ----------------
- * You must complete this function.
- * 
- * @param {number[]} marks - Array of student marks
- * @returns {Object} result object with total, percentage and result
- */
-
 function calculateResult(marks) {
-  // 1. Calculate total marks
   let total = 0;
+
   for (let i = 0; i < marks.length; i++) {
     total += marks[i];
   }
 
-  // 2. Calculate percentage
   let percentage = Math.round((total / (marks.length * 100)) * 100);
 
-  // 3. Decide Pass or Fail
   let result = percentage >= 40 ? "Pass" : "Fail";
 
-  // 4. Return object in the required format
   return {
     total: total,
     percentage: percentage,
